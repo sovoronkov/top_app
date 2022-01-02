4210f5c9f4a79aeb86dd8353d421158f3823294f
s.voronkov@beloil.by <s.voronkov@beloil.by>
2022-01-02 21:44:16 +0300

    1

--- src/ToDo/ToDoItem.js
+++ src/ToDo/ToDoItem.js
@@ -1,13 +1,26 @@
 import React from "react";
 import propTypes from "prop-types";
 
+const styles = {
+    li: {
+      display : 'flex',
+      justifyContent : 'space-between',
+      alignItems : 'center',
+      padding : '.5rem 1rem ',
+      border : '1px solid #ccc',
+      borderRadisu : '4px',
+      marginBottom : '.5rem'
+
+    }
+}
+
 function ToDoItem ({todo}) {
     return (
-        <li>
+        <li style = {styles.li}>
             <span>
                 <input type="checkbox"></input>
             </span>
-            <stromg>{todo.id}</stromg>
+            <strong>{todo.id}</strong>
             {todo.title}
         </li>
     );
