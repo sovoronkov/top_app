1cef815f1ef875c86a73b6f40db36461d2e34476
S.Voronkov <s.voronkov@beloil.by>
2022-01-03 20:32:48 +0300

    1

--- src/ToDo/ToDoItem.js
+++ src/ToDo/ToDoItem.js
@@ -2,33 +2,40 @@ import React from "react";
 import propTypes from "prop-types";
 
 const styles = {
-    li: {
+   li: {
       display : 'flex',
       justifyContent : 'space-between',
       alignItems : 'center',
-      padding : '.5rem 1rem ',
-      border : '1px solid #ccc',
-      borderRadisu : '4px',
+      padding : '.5rem 1rem',
+      border : '2px solid #ccc',
+      borderRadius : '50px',
       marginBottom : '.5rem'
-
+    },
+   input : {
+        marginRight : '1rem',
     }
 }
 
-function ToDoItem ({todo}) {
+function ToDoItem ({todo, onChange}) {
     return (
         <li style = {styles.li}>
             <span>
-                <input type="checkbox"></input>
-            </span>
+                <input type="checkbox" 
+                       style={styles.input} 
+                       onChange={()=>onChange(todo.id)}></input>
             <strong>{todo.id}</strong>
+            &nbsp;
             {todo.title}
+            </span>
+            <button className="rm">&times;</button>
         </li>
     );
 }
 
 ToDoItem.prototype = {
   todo : propTypes.object.isRequired,
-  index : propTypes.number
+  index : propTypes.number,
+  onChange : propTypes.func.isRequired
 }
 
 export default ToDoItem;
